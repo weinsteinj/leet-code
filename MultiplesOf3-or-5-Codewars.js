0e9b68e105fb29d8bab6fe659c4c050140addac3
Josh Weinstein <yamaotoko@gmail.com>
2022-09-20 14:48:09 -0400

    adds Codewars Smash JS

--- MultiplesOf3-or-5-Codewars.js
+++ MultiplesOf3-or-5-Codewars.js
@@ -18,3 +18,22 @@ function solution(number){
   }
   return sum;
 }
+/* Sentence Smash
+   Write a function that takes an array of words and smashes them together
+   into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation,
+   but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning
+   or the end of the sentence!
+
+   Example
+   ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+   */
+function smash (words) {
+  let sentence='';
+  words.forEach(word => sentence += word + ' ')
+  return sentence.trim()
+};
+// alternate solution using Array.prototype.join(optional separator);
+function smash (words) {
+   return words.join(" ");
+};
+
