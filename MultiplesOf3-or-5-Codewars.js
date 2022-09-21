09552918ff68a38e93e80fc2e1f3774c32067165
Josh Weinstein <yamaotoko@gmail.com>
2022-09-20 22:03:20 -0400

    adds Codewars Paperwork JS

--- MultiplesOf3-or-5-Codewars.js
+++ MultiplesOf3-or-5-Codewars.js
@@ -36,4 +36,25 @@ function smash (words) {
 function smash (words) {
    return words.join(" ");
 };
+/*
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+
+*/
+function paperwork(n, m) {
+  let pagesNeeded;
+  if (n >= 0 && m >= 0) {
+      pagesNeeded = n * m;
+      } else {
+      pagesNeeded = 0;
+  }
+  return pagesNeeded;
+}
+//concise solution using ternary operator:
+function paperwork(n, m) {
+  return (n <= 0 || m <= 0) ? 0 : pagesNeeded = n * m;
+}
+
 
